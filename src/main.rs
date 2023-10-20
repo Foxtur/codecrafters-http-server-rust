83ea60b92a070d4767ea454664df2b9cafe21747
Dominik Schenner <admin@foxtur.com>
2023-10-20 03:30:17 +0200

    path handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::io::{Write, Result};
+use std::io::{Write, Result, Read};
 use std::net::{TcpListener, TcpStream};
 
 fn handle_connection(mut stream: TcpStream) -> Result<()>{
     let ok_response = b"HTTP/1.1 200 OK\r\n\r\n";
-    let bytes_written = stream.write(ok_response)?;
-    println!("Wrote {} bytes", bytes_written);
-    stream.flush()?;
+    let not_found_response = b"HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+    let mut buffer = [0; 1024];
+    println!("Reading data from stream");
+    let _bytes_read = stream.read(&mut buffer)?;
+    let received_data = std::str::from_utf8(&buffer).unwrap();
+    println!("Received data: {}", received_data);
+
+    let start_line = received_data.lines().nth(0).unwrap();
+    let path = start_line.split(" ").nth(1).unwrap();
+    println!("Extracted path: {}", path);
+
+    if path == "/" {
+        let bytes_written = stream.write(ok_response)?;
+        println!("Wrote {} bytes", bytes_written);
+        stream.flush()?;
+    } else {
+        let bytes_written = stream.write(not_found_response)?;
+        println!("Wrote {} bytes", bytes_written);
+        stream.flush()?;
+    }
+
     Ok(())
 }
 
